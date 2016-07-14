26ca28b19e55a81347ca25ca80ced847878f2d73
Michael Niepel <michael.niepel@gmail.com>
2016-07-14 17:44:08 +0200

    creates migration for users collection, add unique_constraint to email, ensures unique_constraint(:email) check in changeset function will work

---
+++ priv/repo/migrations/20160714153926_create_user.exs
@@ -0,0 +1,15 @@
+defmodule CountEgo.Repo.Migrations.CreateUser do
+  use Ecto.Migration
+
+  def change do
+    create table(:users) do
+      add :name, :string
+      add :email, :string
+      add :password_hash, :string
+
+      timestamps
+    end
+
+    create unique_index(:users, [:email])
+  end
+end
